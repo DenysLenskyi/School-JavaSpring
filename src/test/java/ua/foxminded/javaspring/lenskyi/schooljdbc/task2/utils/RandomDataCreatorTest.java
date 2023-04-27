44522adb0380216a57bb2a4129289933f6bfd51a
Denys Lenskyi <denislensky94@gmail.com>
2023-04-27 15:57:07 -0400

    RandomDataCreatorTest eternal loading

--- src/test/java/ua/foxminded/javaspring/lenskyi/schooljdbc/task2/utils/RandomDataCreatorTest.java
+++ src/test/java/ua/foxminded/javaspring/lenskyi/schooljdbc/task2/utils/RandomDataCreatorTest.java
@@ -1,20 +1,35 @@
 package ua.foxminded.javaspring.lenskyi.schooljdbc.task2.utils;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+import ua.foxminded.javaspring.lenskyi.schooljdbc.task2.AppConfig;
+import ua.foxminded.javaspring.lenskyi.schooljdbc.task2.Main;
 import ua.foxminded.javaspring.lenskyi.schooljdbc.task2.dao.domain.Group;
 
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
 import java.util.List;
 import java.util.Random;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest
 class RandomDataCreatorTest {
 
-    private Random random = new Random();
-    private FileReader reader = new FileReader();
-    private RandomDataCreator randomDataCreator = new RandomDataCreator(reader, random);
-
     private int numGroups = 10;
+
+
+    @Autowired
+    RandomDataCreator randomDataCreator;
 
 
     @Test
