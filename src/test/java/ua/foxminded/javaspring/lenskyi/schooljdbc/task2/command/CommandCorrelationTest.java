88bc3327b5d7808691cde64bbe94656fd5dc3af2
Denys Lenskyi <denislensky94@gmail.com>
2023-06-12 13:03:55 -0400

    Unknown command test case covered

--- src/test/java/ua/foxminded/javaspring/lenskyi/schooljdbc/task2/command/CommandCorrelationTest.java
+++ src/test/java/ua/foxminded/javaspring/lenskyi/schooljdbc/task2/command/CommandCorrelationTest.java
@@ -1,12 +1,16 @@
 package ua.foxminded.javaspring.lenskyi.schooljdbc.task2.command;
 
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import ua.foxminded.javaspring.lenskyi.schooljdbc.task2.command.commands.*;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 @ActiveProfiles("test")
@@ -16,10 +20,23 @@ class CommandCorrelationTest {
     private CommandCorrelation commandCorrelation;
 
     @ParameterizedTest
-    @ValueSource(strings = {"info", "find_course", "find_groups", "find_students_course", "add_student",
-            "delete_student", "add_student_course", "delete_student_course"})
-    void getCommandCodeTest(String string) {
-        assertEquals("ua.foxminded.javaspring.lenskyi.schooljdbc.task2.command.commands",
-                commandCorrelation.getCommandByCode(string).getClass().getPackageName());
+    @MethodSource("getArgsForComCorTest")
+    void getCommandCodeTest(String string, Class expectedClass) {
+        commandCorrelation.getCommandByCode(string);
+        assertTrue(commandCorrelation.getCommandByCode(string).getClass().getName().contains(expectedClass.getName()));
+    }
+
+    private static Stream<Arguments> getArgsForComCorTest() {
+        return Stream.of(
+                Arguments.of("info", InfoCommand.class),
+                Arguments.of("find_course", FindCourseByIdCommand.class),
+                Arguments.of("find_groups", FindGroupsWithNumStudentsCommand.class),
+                Arguments.of("find_students_course", FindStudentsEnrolledToCourseCommand.class),
+                Arguments.of("add_student", AddStudentCommand.class),
+                Arguments.of("delete_student", DeleteStudentCommand.class),
+                Arguments.of("add_student_course", EnrollStudentToCourseCommand.class),
+                Arguments.of("delete_student_course", RemoveStudentFromCourseCommand.class),
+                Arguments.of("accio_student!", UnknownCommand.class)
+        );
     }
 }
